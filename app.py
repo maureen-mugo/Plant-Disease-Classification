dbc87042142d89db474b73934a20e530999e6d37
maureen <mugomaureenw@gmail.com>
2024-07-16 16:30:48 +0300

    Update changes

--- app.py
+++ app.py
@@ -5,6 +5,7 @@ from PIL import Image
 import requests
 import os
 
+# Function to download and load the model
 @st.cache(allow_output_mutation=True)
 def download_and_load_model():
     model_url = "https://huggingface.co/maureenmugo/Plant_disease_classification/resolve/main/export.pkl"
@@ -20,26 +21,29 @@ def download_and_load_model():
     learn = load_learner(model_path)
     return learn
 
+# Function to resize and classify image
+def classify_img(data):
+    img = PILImage.create(data)
+    # Resize image to 256x256
+    img = img.resize((256, 256))
+    # Disable progress bar
+    with learn_inf.no_bar():
+        pred, pred_idx, probs = learn_inf.predict(img)
+    return pred, probs[pred_idx]
+
+# Streamlit app
 st.title("Plant Disease Detection")
 
 # Description paragraph
 description = """
 This project is trained on tomato, soybean, grape, apple, cassava, coffee, chilli, corn, cherry, guava, cucumber, lemon, 
-mango, jamun, peach, pepper bell, rice, potato, sugarcane, strawberry, tea, wheat, pomegranate plants disease dataset.
-"""
+mango, jamun, peach, pepper bell, rice, potato, sugarcane, strawberry, tea, wheat, pomegranate plants disease dataset.  
+The link to the dataset used to train the model can be accessed here: https://www.kaggle.com/datasets/alinedobrovsky/plant-disease-classification-merged-dataset/data """
 st.write(description)
 
-st.write("Loading model...")
+# st.write("Loading model...")
 learn_inf = download_and_load_model()
 st.write("Model loaded successfully!")
-
-# Classifier
-def classify_img(data):
-    img = PILImage.create(data)
-    # Disable progress bar
-    with learn_inf.no_bar():
-        pred, pred_idx, probs = learn_inf.predict(img)
-    return pred, probs[pred_idx]
 
 # Image uploader
 bytes_data = None
